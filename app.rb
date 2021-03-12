70dcbf02aeabf44cf0960057768431298481ffd4
lycho33 <cho.lyds@gmail.com>
2021-03-12 11:10:44 -0500

    commit tree bf0c954c180fd186048e734e5b5bc1565eb5ba79

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Lydia"
+    end
+
+    get '/hometown' do
+        "My hometown is Maryland"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Traverlers"
+    end
 end
